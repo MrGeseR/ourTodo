d514638a1eb7a77618729fdc6d7ee9c438f93312
Andriy Koval <andris19951995@gmail.com>
2016-08-11 11:46:19 +0300

    Added add-board-function for enter key

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,15 +3,31 @@ import Handlebars from 'handlebars';
 
 
 
-if ($('.btnAddBoard').click || $($('.list-input').))
+var enterBoard = $('.list-input').keypress(function (e) {
+ var key = e.which;
+ if(key == 13)
+  {
+    $('.btnAddBoard').click();
+    return false;
+  }
+});
 
-$('.btnAddBoard').click(function (){
+if ($('.btnAddBoard').click || enterBoard){
+  $('.btnAddBoard').click(function (){
     let val = $('.list-input').val();
     let source   = $('#template-article').html();
     let template = Handlebars.compile(source);
     let context = {title: val};
     let ready = template(context);
     $('#content').html(ready);
-});
+    $('.list-input').val('');
+  });
+};
+
+//////////////////////////////////////////////////////////
+
+
+
+
 
 
